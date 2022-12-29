55b6de553b95e2d8a8bdb06ba45a187631afda07
Vimalesh28 <95900470+Vimalesh28@users.noreply.github.com>
2022-12-30 01:13:24 +0530

    Update JavaIfElse.java

--- JavaIfElse.java
+++ JavaIfElse.java
@@ -10,16 +10,15 @@ public class odd {
 		Scanner sc=new Scanner(System.in);
 		int n=sc.nextInt();
 		if(n%2==0) {
-			if(n>=2 || n<=5) {
-				if(n<=20) {
-					System.out.print("not wired");
-				}
-			}
+		 if(n>=2 || n<=5) {
+		   if(n<=20) {
+		     System.out.print("not wired");
 		}
+	}
+}
 		else{
 			System.out.print("wired");
-
-		}
+                 }
          }
 
 }
